7db90e4523b278449a699a9d3240d76f5659d25a
binhta <binhta102@gmail.com>
2022-03-10 02:40:24 -0500

    bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,6 +72,7 @@ var fight = function(enemy) {
       for (var i = 0; i < enemyInfo.length; i++){
           if(playerInfo.health > 0){
               window.alert("Welcome to Robot Gladiators! Round " +(i+1));
+              //debugger;
               var pickedEnemyObj = enemyInfo[i];
 
               pickedEnemyObj.health = randomNumber(40, 60);
@@ -143,10 +144,19 @@ var shop = function(){
             break;
         }   
     };
+//function to set name
+var getPlayerName = function(){
+   var name = "";
+   while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
 
-
+// GAME INFO and Variables
 var playerInfo = {
-  name : window.prompt ("What is your robot's name?"),
+  name : getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
