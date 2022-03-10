acac7aa4670e2ba49b92dfe236e4b63ee274cfad
binhta <binhta102@gmail.com>
2022-03-10 04:10:08 -0500

    shop option

--- assets/js/game.js
+++ assets/js/game.js
@@ -46,22 +46,7 @@ var fight = function(enemy) {
         }
 
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-
-
-      // if player picks "skip" confirm and then stop the loop
-      if (promptFight === "skip" || promptFight === "SKIP") {
-        // confirm player wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-  
-        // if yes (true), leave fight
-        if (confirmSkip) {
-          window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-          // subtract money from playerInfo.money for skipping
-          playerInfo.money = Math.max(0, playerInfo.money - 10);
-          console.log("playerInfo.money", playerInfo.money)
-          break;
-        }
-      };
+        
       //generate random damage value based on player's attack power
       var damage = randomNumber (playerInfo.attack - 3, playerInfo.attack);
       
@@ -155,30 +140,25 @@ var endGame = function(){
 
 var shop = function(){
      //ask player what they'd like to do
-     var shopOptionPromt = window.prompt(
-         "would you like to REFILL, your health, UPGRADE yout attack, or LEAVE the store? Please enter one: 'REFILL','UPGRADE', or 'LEAVE' to make a choice." 
-        );
-        
-     switch (shopOptionPromt){
-        case "REFILL":
-        case "refill":
-            playerInfo.refillHealth();
-            break;
-        case "UPGRADE": 
-        case "upgrade":
-            playerInfo.upgradeAttack();
-            break;
-        case "LEAVE": //new case
-        case "leave":
-            window.alert("Leaving the store.");
-
-            //do nothing, so function will end
-            break;
-        default:
-            window.alert("You did not pick a valid option, Try agian.");
-            shop();
-            break;
-        }   
+    var shopOptionPromt = window.prompt(
+      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
+    );
+    shopOptionPromt= parseInt(shopOptionPromt);
+    switch (shopOptionPromt){
+      case 1:
+        playerInfo.refillHealth();
+        break;
+      case 2:
+        playerInfo.upgradeAttack();
+        break;
+      case 3:
+        window.alert("Leaving the store.");
+        break;
+      default:
+        window.alert("You did not pick a valid option. Try again.");
+        shop();
+        break;
+      }   
     };
 //function to set name
 var getPlayerName = function(){
